2e0c4706caf31ab5613486097d7340323cd028ba
Kayleigh Hansen <71479913+kayleighhansen@users.noreply.github.com>
2021-11-02 20:49:15 -0600

    ...

--- ScriptureJournal/Pages/Entries/Index.cshtml.cs
+++ ScriptureJournal/Pages/Entries/Index.cshtml.cs
@@ -22,33 +22,37 @@ namespace ScriptureJournal.Pages.Entries
 
         public IList<Entry> Entry { get;set; }
         [BindProperty(SupportsGet = true)]
-        public string SearchString { get; set; }
         public string SearchStringBook { get; set; }
         public SelectList Books { get; set; }
         [BindProperty(SupportsGet = true)]
         public string Book { get; set; }
+
+
+        public string SearchStringText { get; set; }
+        public SelectList Texts { get; set; }
+        [BindProperty(SupportsGet = true)]
         public string Text { get; set; }
 
         public async Task OnGetAsync()
         {
-            IQueryable<string> bookQuery = from m in _context.Entry
-                                                orderby m.Book
-                                                select m.Book;
+            IQueryable<string> bookQuery = from m in _context.Entry orderby m.Book select m.Book;
 
-            var entries = from m in _context.Entry
-                        select m;
+            var entries = from m in _context.Entry select m;
 
-            if (!string.IsNullOrEmpty(SearchString))
-            {
-                entries = entries.Where(s => s.Text.Contains(SearchString));
-            }
-            
             if (!string.IsNullOrEmpty(SearchStringBook))
             {
-                entries = entries.Where(s => s.Text.Contains(SearchString));
+                entries = entries.Where(s => s.Book == SearchStringBook);
             }
 
             Books = new SelectList(await bookQuery.Distinct().ToListAsync());
+            Entry = await entries.ToListAsync();
+
+            if (!string.IsNullOrEmpty(SearchStringText))
+            {
+                entries = entries.Where(s => s.Text == SearchStringText);
+            }
+
+            Texts = new SelectList(await bookQuery.Distinct().ToListAsync());
             Entry = await entries.ToListAsync();
         }
     }
